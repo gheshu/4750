22915ae6b43cdcdf2c840fd108851e729e4ec5f2
gheshu <gheshu@users.noreply.github.com>
2015-09-11 14:32:33 -0500

    Update image.cpp
    clamp x and y to avoid big branches, grab address once in orPixel() to avoid several address translations.

--- image.cpp
+++ image.cpp
@@ -13,16 +13,19 @@ void Image::destroy(){
 }
 
 void Image::setPixel(const int x, const int y, const Pixel& p){
-	if(x >= width || x < 0 || y >= height || y < 0){ return; }
+	x = std::min(width - 1, std::max(0, x));
+	y = std::min(height - 1, std::max(0, y));
 	*(data + x + y * width) = p;
 }
 
 void Image::orPixel(const int x, const int y, const Pixel& p){
-	if(x >= width || x < 0 || y >= height || y < 0){ return; }
-	(data + x + y * width)->r |= p.r;
-	(data + x + y * width)->g |= p.g;
-	(data + x + y * width)->b |= p.b;
-	(data + x + y * width)->a |= p.a;
+	x = std::min(width - 1, std::max(0, x));
+	y = std::min(height - 1, std::max(0, y));
+	Pixel* i = (data + x + y * width);
+	i->r |= p.r;
+	i->g |= p.g;
+	i->b |= p.b;
+	i->a |= p.a;
 }
 
 void Image::clear(const Pixel& p){
